e68fde86b9f322c7f37c332249592a4a74015fb5
originEdu <originpark82@gmail.com>
2026-03-11 11:02:08 +0900

    update c++ init

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+
+using namespace std;
+int main()
+{
+	int A = 10;
+	//P는 별명, 정수형 변수의 주소를 기억하는 숫자 공간을 요청
+	int* P = &A; //A 변수에 주소를 주세요, 정수형 변수A의 주소를 주세요
+	int* P2 = nullptr;
+
+	P2 = P;
+	/*cout << "&A:" << &A << endl;
+	cout << "P:" << P << endl;
+	cout << "P2:" << P2 << endl;*/
+
+	*P2=30; //-> P2라는 변수는 몬지 모르지만 다른 변수의 위치를 기억하는 변수
+	//P2가 가르키는 녀석을 30으로 교체
+	cout << "*P:" << *P << endl;
+	//P가 가르키는 녀석도 30으로 출력됨
+	cout << "*P2:" << *P2 << endl;
+	return 0;
+}
