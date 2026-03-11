e865240d07fee5bb5ae88513a1e167c74773e222
originEdu <originpark82@gmail.com>
2026-03-11 15:14:12 +0900

    Update Code
    1. 함수 헤더로 분리
    2, 함수명 변경 및 주석추가

---
+++ StringUtil.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+
+//문자열 갯수 세기
+int StringLength(char* String, int InputMaxLength);
+
+// 문자열 중에서 특정 문자를 특정문 바꾸기
+void ChangeCertainChar(char* String, int Size, char TargetChar, char ReplaceChar);
+
+//문자열 중에서 특정 문자 위치 찾기
+int* SeachTargetCharPlace(char* String, int Size, char TargetChar);
+
+//특정 문자의 위치를 가진배열 출력
+void PrintPlaceArray(int* PlaceArray, int Size);
--- main.cpp
+++ main.cpp
@@ -1,18 +1,7 @@
 ﻿#include<iostream>
-
+#include "StringUtil.h"
 using namespace std;
 
-//문자열 갯수 세기
-int CountStringLength(char* string);
-
-// 문자열 중에서 특정 문자를 특정문 바꾸기
-void ChangeCertainString(char TargetChar, char ReplaceChar, char* String);
-
-//문자열 중에서 특정 문자 위치 찾기
-int* SeachTargetCharPlace(char TargetChar, char* String);
-
-//특정 문자의 위치를 가진배열 출력
-void PrintPlaceArray(int* PlaceArray);
 
 int main()
 {
@@ -46,96 +35,22 @@ int main()
 
 
 	char test[10] = "abcabbbbc";
-	cout << "CountStringLength: " << CountStringLength(test) << endl;
+	int length = StringLength(test,256); //입력최대글자 256
+	cout << "StringLength: " << length << endl;
 
-	ChangeCertainString('a', 'e',test);
+	ChangeCertainChar(test, length, 'a', 'e');
 	cout << "ChangeCertainString: " << test << endl;
 
 	cout << "SeachTargetCharPlace c:";
-	int* TempArrayPointer = SeachTargetCharPlace('c', test);
-	PrintPlaceArray(TempArrayPointer);
+	int* TempArrayPointer = SeachTargetCharPlace(test, length,'c');
+	PrintPlaceArray(TempArrayPointer, length);
 	cout << endl;
 	delete[] TempArrayPointer;
 
 	cout << "SeachTargetCharPlace s:";
-	int* TempArrayPointer2 =SeachTargetCharPlace('s', test);
-	PrintPlaceArray(TempArrayPointer2);
+	int* TempArrayPointer2 =SeachTargetCharPlace(test, length,'s');
+	PrintPlaceArray(TempArrayPointer2, length);
 	cout << endl;
 	delete[] TempArrayPointer2;
 	return 0;
-}
-
-//문자열 갯수 세기 -> 이게 무슨말일까
-//                    1, "geg", '2', 56, "asqzxc" 이런식으로 저장된 배열에서 문자열의 갯수를 찾는건가?
-// 일단 주어진 문자배열의 길이를 구하는 함수
-int CountStringLength(char* String)
-{
-	int MaxStringLength = 256;  //입력받을 수 있는 문자의 최대길이
-	                            //일단 최대 넘어가는 문자입력 제외
-	int LengthCount = 0;
-	for (int i = 0; i < MaxStringLength; i++)
-	{
-		if (String[i] == '\0')
-		{
-			break;
-		}
-		LengthCount++;
-	}
-	return LengthCount;
-}
-
-//문자열 중에서 특정 문자를 특정문 바꾸기 -> 한글자만 바꾸는 것인지 아니면 문자열로 바꾸는 것인지 확인
-//일단 한글자만 바꾸는 것으로
-void ChangeCertainString(char TargetChar, char ReplaceChar, char* String)
-{
-	int StringLength = CountStringLength(String);
-	for (int i = 0; i < StringLength; i++)
-	{
-		if (String[i] == TargetChar)
-		{
-			String[i] = ReplaceChar;
-		}
-	}
-}
-
-//문자열 중에서 특정 문자 위치 찾기 -> 제일 먼저 찾은 위치 반환할지, 찾은 위치 모두 반환할지
-//일단 제일 먼저 찾은 위치 반환하는 걸로 -> 찾은 위치 모두 반환하는 코드로 변경함
-int* SeachTargetCharPlace(char TargetChar, char* String)
-{
-	int StringLength = 0; 
-	StringLength = CountStringLength(String);
-	int* PlaceArray = new int[256]; //찾은 문자 위치 저장하는 배열, 일단 문자 최대값 -> 나중에 동적할당
-	int ArrayIndex = 0; //PlaceArray의 인덱스
-	for (int i = 0; i < 256; i++) //찾는 것이 없을시 구분 + 출력을 위해 모든값에 -1 삽입
-	{
-		PlaceArray[i] = -1;
-	}
-	
-	for (int i = 0; i < StringLength; i++)
-	{
-		if (String[i] == TargetChar)
-		{
-			PlaceArray[ArrayIndex] = i + 1;
-			ArrayIndex++;
-		}
-	}
-	
-	return PlaceArray;
-}
-
-//찾은 문자 위치 저장하는 배열 출력 함수
-void PrintPlaceArray(int* PlaceArray)
-{
-
-	for (int i = 0; i < 256;i++) //최대길이 256이니깐
-	{
-		if (PlaceArray[i] == -1)
-		{
-			break;
-		}
-		else 
-		{
-			cout << " " << PlaceArray[i];
-		}
-	}
 }
